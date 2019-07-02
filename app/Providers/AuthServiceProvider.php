66c7b9033aeefccb8a669bf53cc4924058a6eb6a
nicolasey <nicolas.sey@gmail.com>
2019-07-02 22:34:43 +0200

    enable cache in prod

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Bouncer;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        if(getenv("APP_ENV") === "production") {
+            Bouncer::cache();
+        } else {
+            Bouncer::dontCache();
+        }
     }
 }
