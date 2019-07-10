342d2706dd6465f542176c7927925045bc271f73
nicolasey <nicolas.sey@gmail.com>
2019-07-10 12:52:02 +0200

    wip forum tests

--- database/factories/PersonnageFactory.php
+++ database/factories/PersonnageFactory.php
@@ -4,10 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(\Nicolasey\Personnage\Models\Personnage::class, function (Faker $faker) {
     static $name;
-    static $owner;
+    static $owner_id;
 
     return [
         'name' => ($name) ? $name : $faker->name,
-        'owner' => ($owner) ? $owner : 1,
+        'owner_id' => ($owner_id) ? $owner_id : factory(\App\User::class)->create()->id,
     ];
 });
--- modules/forum/tests/ForumTest.php
+++ modules/forum/tests/ForumTest.php
@@ -2,26 +2,56 @@
 
 namespace Modules\Forum\Tests;
 
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Modules\Forum\Models\Forum;
+use Modules\Forum\Models\Topic;
+use Nicolasey\Personnages\Models\Personnage;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ForumTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations;
 
-        $response->assertStatus(200);
+    private $forum;
+    private $forum_topic;
+    private $author;
+    private $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->forum = factory(Forum::class)->create();
+        $this->user = factory(User::class)->create();
+        /*$this->author = factory(Personnage::class)->create([
+            "owner_id" => $this->user->id
+        ]);
+        $this->forum_topic = factory(Topic::class)->create([
+            "author" => $this->author->id,
+            "forum_id" => $this->forum->id
+        ]);*/
     }
 
+    /**
+     * @test
+     */
     public function it_stores_a_forum()
     {
+        $response = $this->post("api/forums", [
+            "name" => "small forum",
+            "content" => "",
+            "parent_id" => null
+        ]);
 
+        $response->assertSuccessful();
+
+        $this->assertDatabaseHas("forums", [
+            "name" => "small forum",
+            "slug" => "small-forum",
+            "content" => null,
+            "parent_id" => null
+        ]);
     }
 
     public function it_updates_a_forum()
@@ -34,14 +64,23 @@ class ForumTest extends TestCase
 
     }
 
+    /**
+     * @test
+     */
     public function it_destroys_forum_softly()
     {
-        
+        $response = $this->delete("api/forums/".$this->forum->id);
+        $response->assertSuccessful();
+        $this->assertSoftDeleted("forums", ["id" => $this->forum->id]);
     }
 
+    /**
+     * @todo
+     */
     public function it_destroys_child_topics_when_deleted()
     {
-        
+        $this->delete("api/forums/".$this->forum->id);
+        $this->assertSoftDeleted("topics", ["id" => $this->forum_topic->id]);
     }
 
     public function it_destroys_child_forums_when_deleted()
