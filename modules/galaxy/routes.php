7c66456f1d469e9a9eb8e83de0ddf04f743e9844
nicolasey <nicolas.sey@gmail.com>
2019-07-13 08:50:48 +0200

    fix planet routing

--- modules/galaxy/routes.php
+++ modules/galaxy/routes.php
@@ -1,6 +1,6 @@
 <?php
 Route::apiResource("secteurs", "SecteursController");
-Route::apiResource("secteurs.planets", "SecteursController");
+Route::apiResource("secteurs.planets", "PlanetsController");
 Route::get("planets/{planet}/managers/{personnage}", "PlanetsController@setAsManager")
     ->name("planet.setManager");
 Route::delete("planets/{planet}/managers/{personnage}", "PlanetsController@removeAsManager")
