ac00cb9a5c28b2fb25ed08d78c58d5bb900230f9
nicolasey <nicolas.sey@gmail.com>
2019-07-14 19:25:39 +0200

    fix routing

--- modules/economy/routes.php
+++ modules/economy/routes.php
@@ -1,3 +1,3 @@
 <?php
-Route::post("api/transactions", "\Modules\Economy\Actions\MakeTransaction")->name('transaction.make');
-Route::delete("api/transactions", "\Modules\Economy\Actions\CancelTransaction")->name('transaction.cancel');
+Route::post("transactions", "\Modules\Economy\Actions\MakeTransaction")->name('transaction.make');
+Route::delete("transactions", "\Modules\Economy\Actions\CancelTransaction")->name('transaction.cancel');
