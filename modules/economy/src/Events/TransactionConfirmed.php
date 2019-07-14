29f4f93eb6f2efb85f88f532fd59339ba6eeed1f
nicolasey <nicolas.sey@gmail.com>
2019-07-14 18:49:27 +0200

    add events

---
+++ modules/economy/src/Events/TransactionConfirmed.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\Economy\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Modules\Economy\Models\Transaction;
+
+class TransactionConfirmed
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $transaction;
+
+    /**
+     * TransactionConfirmed constructor.
+     *
+     * @param Transaction $transaction
+     * @return void
+     */
+    public function __construct(Transaction $transaction)
+    {
+        $this->transaction = $transaction;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
