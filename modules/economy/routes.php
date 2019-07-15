580f059677e498e551cb5ca5dc3e4ab9fa477c00
nicolasey <nicolas.sey@gmail.com>
2019-07-15 11:20:55 +0200

    add fiche management
    use economy lines to get you economic fiche updated

--- modules/economy/routes.php
+++ modules/economy/routes.php
@@ -1,3 +1,4 @@
 <?php
 Route::post("transactions", "\Modules\Economy\Actions\MakeTransaction")->name('transaction.make');
 Route::delete("transactions", "\Modules\Economy\Actions\CancelTransaction")->name('transaction.cancel');
+Route::apiResource("fiches.economy-lines", "EconomyLineController");
