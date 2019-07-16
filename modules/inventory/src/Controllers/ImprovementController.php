bfbeb8ad82b675618224e4eee8ee9e87b6ed6338
nicolasey <nicolas.sey@gmail.com>
2019-07-16 15:10:53 +0200

    add object improvements ctrl

--- modules/inventory/src/Controllers/ImprovementController.php
+++ modules/inventory/src/Controllers/ImprovementController.php
@@ -1,10 +1,75 @@
 <?php
+namespace Modules\Inventory\Controllers;
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Modules\Inventory\Models\Improvement;
+use Modules\Inventory\Models\ObjectType;
 
 class ImprovementController extends Controller
 {
-    //
+    public function index(ObjectType $objectType)
+    {
+        return $objectType->improvements()->get();
+    }
+
+    public function show(ObjectType $objectType, Improvement $improvement)
+    {
+        $improvement->type = $objectType;
+        return $improvement;
+    }
+
+    /**
+     * Create improvement
+     *
+     * @param ObjectType $objectType
+     * @return \Illuminate\Database\Eloquent\Model
+     * @throws \Exception
+     */
+    public function store(ObjectType $objectType)
+    {
+        $data = request()->only(["name"]);
+
+        try {
+            $improvement = $objectType->improvements()->create($data);
+            return $improvement;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    /**
+     * @param ObjectType $objectType
+     * @param Improvement $improvement
+     * @return \Illuminate\Database\Eloquent\Model|Improvement
+     * @throws \Exception
+     */
+    public function update(ObjectType $objectType, Improvement $improvement)
+    {
+        $data = request()->only(["name", "object_type_id"]);
+
+        try {
+            $improvement->update($data);
+            return $improvement;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    /**
+     * Delete improvement
+     *
+     * @param ObjectType $objectType
+     * @param Improvement $improvement
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function destroy(ObjectType $objectType, Improvement $improvement)
+    {
+        try {
+            $improvement->delete();
+            return response()->json();
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
 }
