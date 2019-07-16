b2c0d08edd164dcc6fa18f7acde965ae5cf25e5c
nicolasey <nicolas.sey@gmail.com>
2019-07-16 13:12:02 +0200

    add routes

--- modules/inventory/routes.php
+++ modules/inventory/routes.php
@@ -1,1 +1,5 @@
 <?php
+Route::apiResource('object-types', "ObjectTypeController");
+Route::apiResource('object-types.objects', "ObjectController");
+Route::apiResource('object-types.improvements', "ImprovementController");
+Route::apiResource('possessions', "PossessionController");
