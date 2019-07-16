90239879c9ad5c0dae897fa7623593d0b44660d8
nicolasey <nicolas.sey@gmail.com>
2019-07-16 15:11:28 +0200

    del possession from routes for now

--- modules/inventory/routes.php
+++ modules/inventory/routes.php
@@ -2,4 +2,3 @@
 Route::apiResource('object-types', "ObjectTypeController");
 Route::apiResource('object-types.objects', "ObjectController");
 Route::apiResource('object-types.improvements', "ImprovementController");
-Route::apiResource('possessions', "PossessionController");
