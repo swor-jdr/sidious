ee0e6c20058311bdd754a48c2f9ff3f7b387f4d4
Nicolas Seynaeve <nicolas.sey@gmail.com>
2019-11-23 13:42:00 +0100

    fix missing groups config
    w

---
+++ modules/factions/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "inFactions" => [
+        "personnages" => \Modules\Personnages\Models\Personnage::class,
+        "planets" => \Modules\Galaxy\Models\Planet::class,
+    ]
+];
