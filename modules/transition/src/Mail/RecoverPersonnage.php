a7f691762cdb1b39fc6f5a2ccede95f67d4aed43
Nicolas Seynaeve <nicolas.sey@gmail.com>
2020-02-20 15:54:48 +0100

    send mail with personnage object

--- modules/transition/src/Mail/RecoverPersonnage.php
+++ modules/transition/src/Mail/RecoverPersonnage.php
@@ -5,19 +5,22 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Modules\Personnages\Models\Personnage;
 
 class RecoverPersonnage extends Mailable
 {
     use Queueable, SerializesModels;
+
+    public $personnage;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Personnage $personnage)
     {
-        //
+        $this->personnage = $personnage;
     }
 
     /**
