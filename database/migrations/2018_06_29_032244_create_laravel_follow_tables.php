b02a6cb4d11d8813b2fa0381841f89b6e0490d29
Nicolas Seynaeve <nicolas.sey@gmail.com>
2020-02-25 11:58:26 +0100

    add follow pack

---
+++ database/migrations/2018_06_29_032244_create_laravel_follow_tables.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the overtrue/laravel-follow
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaravelFollowTables extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create(config('follow.followable_table', 'followables'), function (Blueprint $table) {
+            $userForeignKey = config('follow.users_table_foreign_key', 'user_id');
+            $table->unsignedBigInteger($userForeignKey)->index();
+            $table->unsignedInteger('followable_id')->index();
+            $table->string('followable_type')->index();
+            $table->string('relation')->default('follow')->comment('follow/like/subscribe/favorite/upvote/downvote');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop(config('follow.followable_table', 'followables'));
+    }
+}
