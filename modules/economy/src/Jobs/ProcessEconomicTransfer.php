6c9b9fc9d5eac5c6e7164d97c87012a975cfe64d
nicolasey <nicolas.sey@gmail.com>
2020-05-19 16:24:47 +0200

    rm useless import

--- modules/economy/src/Jobs/ProcessEconomicTransfer.php
+++ modules/economy/src/Jobs/ProcessEconomicTransfer.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Modules\Economy\Actions\MakeTransaction;
 use Modules\Economy\Events\TransactionOrder;
 use Modules\Economy\Models\Account;
 
