1cb0dd139178425413c0fb4a27e6f25e0126d60f
nicolasey <nicolas.sey@gmail.com>
2020-05-23 14:31:23 +0200

    fix author in topic, post factories

--- modules/forum/database/factories/TopicFactory.php
+++ modules/forum/database/factories/TopicFactory.php
@@ -3,11 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(\Modules\Forum\Models\Topic::class, function (Faker $faker) {
-    static $author;
+    static $author_id;
     static $forum_id;
 
     return [
-        "author" => ($author) ? $author : factory(\Nicolasey\Personnages\Models\Personnage::class)->create()->id,
+        "author_id" => ($author_id) ? $author_id : factory(\Modules\Personnages\Models\Personnage::class)->create()->id,
         "forum_id" => ($forum_id) ? $forum_id : factory(\Modules\Forum\Models\Forum::class)->create()->id,
         "name" => $faker->sentence,
         "content" => null
