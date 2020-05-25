cd81089c47b51069a77395287314f618d7aef00a
nicolasey <nicolas.sey@gmail.com>
2020-05-25 11:18:59 +0200

    fix not working routes

--- modules/economy/routes.php
+++ modules/economy/routes.php
@@ -1,10 +1,8 @@
 <?php
-Route::post("transactions", "\Modules\Economy\Actions\MakeTransaction")->name('transaction.make');
-Route::delete("transactions", "\Modules\Economy\Actions\CancelTransaction")->name('transaction.cancel');
 Route::apiResource("fiches.economy-lines", "EconomyLineController")->middleware('auth:api');
 Route::get("fiches/{fich}/economy-lines/{number}/validate", "EconomyLineController@validation")->middleware('auth:api');
 Route::get("fiches/{personnage}", "FicheController@show")->name('fiche.show');
-Route::post("accounts/complex", "AccountController@complexTransaction")->name('account.complexTransaction');
+Route::post("accounts/complex", "AccountController@complexTransaction")->name('account.complexTransaction')->middleware('auth:api');
 Route::get("accounts/{account}", "AccountController@show")->name('account.show');
-Route::post("accounts/{account}", "AccountController@fromAccountToAnother")->name('account.fromAccountToAnother');
-Route::get("accounts/{account}/freeze", "AccountController@freeze")->name('account.freeze');
+Route::post("accounts/{account}", "AccountController@fromAccountToAnother")->name('account.fromAccountToAnother')->middleware('auth:api');
+Route::get("accounts/{account}/freeze", "AccountController@freeze")->name('account.freeze')->middleware('auth:api');
